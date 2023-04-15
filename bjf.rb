2e4454ae6b2710048a4f406bed023e92134b4000
dvlproad <dvlproad@163.com>
2023-04-16 03:35:28 +0800

    【Fix】修复Formula类名

--- bjf.rb
+++ bjf.rb
@@ -1,5 +1,5 @@
-class BranchJSONFile < Formula
-  desc "分支json文件的创建"
+class BJF < Formula
+  desc "BranchJSONFile:分支json文件的创建"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
   url "https://github.com/dvlpCI/script-branch-json-file/archive/0.0.3.tar.gz"
   sha256 "95fe968314e93a3a08ce18f641c21d3b684f4122ad0e076e2030236194ee2235"

5ee3b3b355f4edd4e29f1d6b0f8e873d9acd0523
dvlproad <dvlproad@163.com>
2023-04-21 03:06:02 +0800

    【Fix】sha256

--- qtool.rb
+++ qtool.rb
@@ -2,7 +2,7 @@ class Qtool < Formula
   desc "QTool:含分支json文件的创建+Jenkins打包"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
   url "https://github.com/dvlpCI/script-branch-json-file/archive/0.1.0.tar.gz"
-  sha256 "1d028a9ae638997815d7540650f3ae45de88ba99bd4c97c57642f6b051862449"
+  sha256 "323ba019e1b35fa3fdbc238420a1b8eca8f0df389a1a21738b1f5965fbaf6373"
   # shasum -a 256 xxxx.tar.gz
   # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
 

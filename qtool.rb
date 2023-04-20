06f85ae7ad9011b034faefd01b6eaf9d80350aed
dvlproad <dvlproad@163.com>
2023-04-20 19:20:55 +0800

    【Feature】使用qtool统一

--- qtool.rb
+++ qtool.rb
@@ -1,26 +1,26 @@
-class Bjf < Formula
-  desc "BranchJSONFile:分支json文件的创建"
+class Qtool < Formula
+  desc "QTool:含分支json文件的创建+Jenkins打包"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
-  url "https://github.com/dvlpCI/script-branch-json-file/archive/0.0.4.tar.gz"
-  sha256 "4b4571ea6d505ac55d8609dcb77b853934222dc3861d89ffc6e86d9d20accf97"
-  # version "0.0.4"
+  url "https://github.com/dvlpCI/script-branch-json-file/archive/0.0.5.tar.gz"
+  sha256 "1af711d4c34cb457f776638557d2de61bde16981532eba558051cc5987c0d2ae"
+  # version "0.0.5"
   # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
 
   def install
     # Install script to bin
     # bin.install "helloworld.sh"
-    bin.install "bjf"
+    bin.install "qtool"
     lib.install Dir["*"] # 记得添加lib目录，并将源码存放于lib里的src下
   end
 
 
   def uninstall
     # rm "#{bin}/helloworld.sh"
-    rm "#{bin}/bjf"
+    rm "#{bin}/qtool"
   end
 
   test do
     # Test your script
-    system "#{bin}/bjf", "--version"
+    system "#{bin}/qtool", "--version"
   end
 end

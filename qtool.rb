6ea7a3d43ba2f326ad20906fa23f23802f410c78
dvlproad <dvlproad@163.com>
2023-04-23 11:11:29 +0800

    【Feature】0.1.3

--- qtool.rb
+++ qtool.rb
@@ -1,8 +1,8 @@
 class Qtool < Formula
   desc "QTool:含分支json文件的创建+Jenkins打包"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
-  url "https://github.com/dvlpCI/script-branch-json-file/archive/0.1.2.tar.gz"
-  sha256 "5b91dd1ee9f345616317aba8776a8e52c58d5b57336c867dd8c66fc0c85cea90"
+  url "https://github.com/dvlpCI/script-branch-json-file/archive/0.1.3.tar.gz"
+  sha256 "72b9e408fe32c5cfa1b803ed6647a0eff298380f62481a5fa04572f467c9741a"
   # shasum -a 256 xxxx.tar.gz
   # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
 

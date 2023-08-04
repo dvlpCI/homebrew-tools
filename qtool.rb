742456415a8d4e2a81440ed257f0556658b6d401
dvlproad <dvlproad@163.com>
2023-08-04 18:58:26 +0800

    【Fix】修复shell下08是八进制问题

--- qtool.rb
+++ qtool.rb
@@ -1,8 +1,8 @@
 class Qtool < Formula
   desc "QTool:含分支json文件的创建+Jenkins打包"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
-  url "https://github.com/dvlpCI/script-branch-json-file/archive/0.5.9.tar.gz"
-  sha256 "4d71c414959372067c74dc5412250493d04b9fbef36662c9babbced786e8499c"
+  url "https://github.com/dvlpCI/script-branch-json-file/archive/0.6.0.tar.gz"
+  sha256 "31aa9a51ae7da59ce9022ba7f4f33cf3c220678ea278c27503e280e933be37c1"
   # shasum -a 256 xxxx.tar.gz
   # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
 

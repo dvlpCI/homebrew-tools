5d42e560a94c868f13402b5151298e6725c09b5c
dvlproad <dvlproad@163.com>
2026-05-18 03:48:20 +0800

    【Update】qtool 0.9.21 → 0.9.23

--- qtool.rb
+++ qtool.rb
@@ -2,11 +2,11 @@ class Qtool < Formula
   desc "QTool:含分支json文件的创建+Jenkins打包"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
 
-  version "0.9.21"
+  version "0.9.23"
   # revision 1  # 版本号不变，但 revision 递增，会触发升级
   url "https://github.com/dvlpCI/script-branch-json-file/archive/#{version}.tar.gz"
   # url "https://github.com/dvlpCI/script-branch-json-file/archive/0.9.13.tar.gz"
-  sha256 "2daf94ca58e0d449bc1b294d2ec6fa35679eb4fcb39eff69dc7fcc4ca34de1cf"
+  sha256 "9ec596a7328109322e53c995d2f3995a16c65ffeec6c7173ece0ca508602adb5"
   # shasum -a 256 xxxx.tar.gz
   # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
 
